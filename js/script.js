fa8545e34dafcd4fc0584bbab0bcee3ed723e493
Matt Pestridge <mpestridge1986@gmail.com>
2016-11-01 16:36:56 -0600

    Updated ingredient dropdown lists

--- js/script.js
+++ js/script.js
@@ -2,15 +2,18 @@
 $(document).ready(function() {
   $('select').material_select();
 
-// var edamamAPI = "https:api.edamam.com/api/nutrition-details?app_id=${8ec7dafd}&app_key=${10eb87abcfa912a8ad8daef79698acb8}";
-//
-// (function () {
-// var testResults = $.getJSON(edamamAPI, {
-//     id: "recipe-title",
-//     title: "Fresh Ham Roasted With Rye Bread and Dried Fruit Stuffing",
-//     format: "json"
-//   });
-//   console.log(testResults);
-// });
+var edamamAPI = "https:api.edamam.com/api/nutrition-details?app_id=8ec7dafd&app_key=10eb87abcfa912a8ad8daef79698acb8";
+
+function test() {
+var testResults = $.getJSON(edamamAPI, {
+    contentType: "application/json",
+    title: JSON.stringify("ham"),
+    ingr: JSON.stringify(["ham"]),
+    format: "json"
+  });
+  console.log(testResults);
+  console.log("This function is running");
+};
+test();
 
 }); // end for document ready //
