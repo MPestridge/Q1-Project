c911a99c9a961c31294c14f6db2c984a080588d1
Matt Pestridge <mpestridge1986@gmail.com>
2016-11-01 15:02:41 -0600

    layout is close enough to start working on talking to the API

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-"use strict"
+"use strict";
 $(document).ready(function() {
   $('select').material_select();
-});
-$('select').material_select('destroy');
+
+// var edamamAPI = "https:api.edamam.com/api/nutrition-details?app_id=${8ec7dafd}&app_key=${10eb87abcfa912a8ad8daef79698acb8}";
+//
+// (function () {
+// var testResults = $.getJSON(edamamAPI, {
+//     id: "recipe-title",
+//     title: "Fresh Ham Roasted With Rye Bread and Dried Fruit Stuffing",
+//     format: "json"
+//   });
+//   console.log(testResults);
+// });
+
+}); // end for document ready //
