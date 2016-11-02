4f0b778a2118ad879f30e30fcd01efe228bebc56
Matt Pestridge <mpestridge1986@gmail.com>
2016-11-02 13:58:16 -0600

    checked ingredients are correctly being added to end of API URL

--- js/script.js
+++ js/script.js
@@ -2,31 +2,41 @@
 $(document).ready(function() {
   $('select').material_select();
 
-  var ingredientArr =[];
-  function grabIngredients() {
-    var checkedIngredients = $('select').material_select();
-    ingredientArr.push(checkedIngredients);
-  }
-  grabIngredients();
-  console.log(ingredientArr);
-
 var edamamAPI = "https://g-edamam-matt-pestridge.herokuapp.com/search?q=";
 
-function recipeSearch() {
-  var newURL = "";
-  for (var i=0; i<ingredientArr.length; i++) {
-    if (ingredientArr.length < 1) {
-      newURL = edamamAPI + ingredientArr[i];
-    } else {
-      newURL = edamamAPI + ingredientArr.join("+");
-    }
-  }
-var objResults = $.getJSON(newURL, {
-    contentType: "application/json",
-    format: "json"
-  });
-  console.log(objResults);
+$("#submit").click(function() {
+  var proteinsArr = $("#proteins").val();
+  var veggiesArr = $("#veggies").val();
+  var grainsArr = $("#grains").val();
+  var saucesArr = $("#sauces").val();
+  var dairyArr = $("#dairy").val();
+  var ingredientArr =[proteinsArr+"+"+veggiesArr+"+"+grainsArr+"+"+saucesArr+"+"+dairyArr];
+  function joinedArr(arr) {
+    return arr.join();
 }
-recipeSearch();
+  // joinedArr(ingredientArr) returns a single array of checked ingredients //
+  var ingrString = joinedArr(ingredientArr);
+  function recipeSearch() {
+    function urlUpdate() {
+      var newURL = "";
+    for (var i=0; i<ingrString.length; i++) {
+      if (ingrString.length < 1) {
+        newURL = edamamAPI + ingrString[i];
+      } else {
+        newURL = edamamAPI + ingrString.replace(/,/g , "+");
+      }
+    }
+    console.log(newURL);
+    return newURL;
+  }
+  // adds selected ingredients to the end of the website url //
 
+  var objResults = $.getJSON(urlUpdate(), {
+      contentType: "application/json",
+      format: "json"
+    });
+    console.log(objResults);
+  }
+  recipeSearch();
+  });
 }); // end for document ready //
