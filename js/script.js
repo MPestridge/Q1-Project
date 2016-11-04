36714a977f285523eb54e45ce668063829dd9f88
Matt Pestridge <mpestridge1986@gmail.com>
2016-11-04 13:31:36 -0600

    added spaces between health list items

--- js/script.js
+++ js/script.js
@@ -14,10 +14,11 @@ $("#clr-button").click(function() {
 
 select.material_select();
 
-$(".input-field").mouseleave(function() {
-  $("input .select-dropdown").removeClass(".active");
-  console.log("the mouse left the box");
-});
+// NOTE: code below is a possible solution for boxes not closing automatically on smaller devices
+// $(".input-field").mouseleave(function() {
+//   $("input .select-dropdown").removeClass(".active");
+//   console.log("the mouse left the box");
+// });
 
 $("#submit").click(function() {
   var proteinsArr = $("#proteins").val();
@@ -55,17 +56,18 @@ $("#submit").click(function() {
         Materialize.toast('You have selected items that do not return any results! Please select different ingredients.', 6000, "rounded");
       } else {
       for (var i=0; i<recipeObjs.length; i++) {
+        console.log((recipeObjs[i].recipe.healthLabels).join(', '));
         if(i % 2 === 0) {
         $("#left-column").append($("<img class='inner" + i + "' src=" + recipeObjs[i].recipe.image + ">"));
         $("#left-column").append($("<span style='text-shadow: 2px 2px #212121;' class='card-title inner" + i + "'>" +recipeObjs[i].recipe.label + "</span>"));
         $(".inner" + i).wrapAll("<div class='row outer-container card" + i + "'><div class='col s12 crdcol" + i + "'><div class='card medium mcrd" + i + "'><div class='card-image crdimg" + i + "'></div></div></div></div>");
-        $(".crdimg" + i).after(("<div class='card-content crdcont" + i + "'><p>" + recipeObjs[i].recipe.healthLabels + "</p></div>"));
+        $(".crdimg" + i).after(("<div class='card-content crdcont" + i + "'><p>" + (recipeObjs[i].recipe.healthLabels).join(", ") + "</p></div>"));
         $(".crdcont" + i).after("<div class='card-action crdact" + i + "'><a href='" + recipeObjs[i].recipe.shareAs + "' target='_blankd'>View Recipe</a></div>");
       } else if (i % 2 !== 0) {
         $("#right-column").append($("<img class='inner" + i + "' src=" + recipeObjs[i].recipe.image + ">"));
         $("#right-column").append($("<span style='text-shadow: 2px 2px #212121;' class='card-title inner" + i + "'>" +recipeObjs[i].recipe.label + "</span>"));
         $(".inner" + i).wrapAll("<div class='row outer-container card" + i + "'><div class='col s12 crdcol" + i + "'><div class='card medium mcrd" + i + "'><div class='card-image crdimg" + i + "'></div></div></div></div>");
-        $(".crdimg" + i).after(("<div class='card-content crdcont" + i + "'><p>" + recipeObjs[i].recipe.healthLabels + "</p></div>"));
+        $(".crdimg" + i).after(("<div class='card-content crdcont" + i + "'><p>" + (recipeObjs[i].recipe.healthLabels).join(", ") + "</p></div>"));
         $(".crdcont" + i).after("<div class='card-action crdact" + i + "'><a href='" + recipeObjs[i].recipe.shareAs + "' target='_blankd'>View Recipe</a></div>");
       }
       }
