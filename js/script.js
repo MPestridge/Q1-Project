36180082c389b5ea66cdef67ecb2404624ad27bf
Matt Pestridge <mpestridge1986@gmail.com>
2018-07-21 11:37:58 -0600

    removed some notes in the javascript file

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 $(document).ready(function() {
   $('select').formSelect();
 
-// EXAMPLE URL: https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free
 var edamamAPI = "https://api.edamam.com/search?q=";
 
 var select = $('select');
